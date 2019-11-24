4902f1de78d4ddc296426cf16162febb56672850
Vinicius Dias <vdias6849@gmail.com>
2019-11-24 15:54:52 -0300

    agora so pega o stream uma vez, que é ao carregar a página

--- scriptElementos.js
+++ scriptElementos.js
@@ -1,43 +1,54 @@
+// Ao carregar a página, aguarda 3 segundos e muda o elemento pra mostrar o peer.id e 
+//chama a funcao pra pegar o stream atual
 setTimeout(() => {
+
   document.getElementById("idPeer").innerHTML ="Seu id: " + peer.id;
   console.log("Id peer atual:", peer.id);
   pegaStream();
-}, 3000);	
+}, 3000
+
+);	
 
 
-// conectar modal, códigos para manipular a conexão
+// Chama a funcao de conectar com idReceptor informado como parâmetro
 $( "#connect" ).click(function( event ) {
+
   var idReceptor = $('#form-appid').val();
   conectar(idReceptor);
-  
+
 });
 
+// Conecta o peer atual ao peer especificados
 function conectar(idReceptor){
+
   console.log("conectando ao id:", idReceptor);
   conn = peer.connect(idReceptor);
   conn.on('open', function () {
     chamar(idReceptor); //chama a função pra ligar
   });
-  
-  $("#modalForm").modal("hide");
-}
+  $("#modalForm").modal("hide"); // Esconde o modal 
 
-// UI buttons
+};
+
+// Ativa botoões (chamada após conexão efetuada)
 function enableUiControls() {
 
   $("#mic-btn").prop("disabled", false);
   $("#video-btn").prop("disabled", false);
   $("#exit-btn").prop("disabled", false);
+
 }
 
+// Manipula botao de microfone
 $("#mic-btn").click(function(){
 
   toggleBtn($("#mic-btn")); // altera a cor do botao microfone
   $("#mic-icon").toggleClass('fa-microphone').toggleClass('fa-microphone-slash'); // altera o icone para cortado
   enviaDados('audio '+$("#mic-icon").hasClass('fa-microphone'));
-  
+
 });
 
+// Manipula botão de vídeo
 $("#video-btn").click(function(){
 
   toggleBtn($("#video-btn")); // altera a cor do botao video
@@ -47,47 +58,51 @@ $("#video-btn").click(function(){
 
 });
 
-$("#exit-btn").click(function(){ // desonectar e forçar a desconexão com o peer no qual está conectado
+// Desconectar e forçar a desconexão com o peer no qual está conectado
+$("#exit-btn").click(function(){ 
 
   enviaDados("desconectar");
   peer.disconnect();
   location.reload();
+
 });
 
+// Altera a cor do botão passado para vermelho
 function toggleBtn(btn){
+
   btn.toggleClass('btn-dark').toggleClass('btn-danger');
-}
 
-function toggleVisibility(elementID, visible) {
-  if (visible) {
-    $(elementID).attr("style", "display:block");
-  } else {
-    $(elementID).attr("style", "display:none");
-  }
-}
+};
 
+// Ativar/Desativar o vídeo local (pop-up de video)
 function toggleVideoLocal(st) {
   
   if ($("#video-icon").hasClass('fa-video')) {
-    st.getVideoTracks()[0].enabled = true;  // enable the local video
+    st.getVideoTracks()[0].enabled = true; 
   } else {
-    st.getVideoTracks()[0].enabled = false; // disable the local video
+    st.getVideoTracks()[0].enabled = false; 
   }
-}
 
+};
+
+// Ativar/Desativar o microfone global (do emissor)
 function toggleMicGlobal(st,cond) {
-  
-  st.getAudioTracks()[0].enabled = cond; // enable/disable the global mic
-  
-}
 
+  st.getAudioTracks()[0].enabled = cond; 
+
+};
+
+// Ativar/Desativar o video global (do emissor)
 function toggleVideoGlobal(st,cond) {
 
-  st.getVideoTracks()[0].enabled = cond; // enable/disable the global video
-  
+  st.getVideoTracks()[0].enabled = cond;
+
 }
 
+// Desconectar de todos os peers conectados
 function disconnect(){
+
   peer.disconnect();
   location.reload();
+
 }
--- scriptPeerJs.js
+++ scriptPeerJs.js
@@ -5,35 +5,32 @@ let peer = new Peer();
 let myStream;
 let conStream;
 
+// Verificacao para manter a compatibilidade, cada navegador tem seu jeito de pegar
 navigator.getUserMedia = navigator.getUserMedia || navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia;
 
+// Pegar o audio/video do peer atual
 function pegaStream(){
   navigator.getUserMedia({ video: true, audio: true }, function (stream) {
     myStream = stream;
   });
 }
 
-function isHidden(el) {
-  return (el.offsetParent === null)
-}
-
 //chamada de video
 function chamar(idReceptor){
-
   peer.call(idReceptor, myStream);
-
 };
 
-//mandar mensagens
+// envio de dados
 const enviaDados = (dados) => {
   conn.send(dados);
 }
 
-// itera para o peer continuar recebendo!
+// constante verificacao pra ver se tem alguma coisa no buffer de entrada
 peer.on('connection', function (conn) {
   conn.on('data', function (data) {
-    console.log(data);
-    switch (data){
+    
+    // Verificacao pra poder ativar/desativar o áudio/video do emissor 
+    switch (data){ 
       case 'video true' :
         toggleVideoGlobal(conStream,true);
         break;
@@ -62,34 +59,30 @@ peer.on('call', function (call) {
   for (var conexao in peer.connections){
       status = conexao;
   }
-
   if (status == 'conectado'){
     console.log(status);
   }
   else{
     var conexaoAtual;
-    for (var conexao in peer.connections){
+    for (var conexao in peer.connections){ //pegar o id do emissor nas conxoes que ele possui
       conexaoAtual = conexao;
     }
-    peer._connections.set('conectado')
+    peer._connections.set('conectado') //adiciona o indice 'conectado' no objeto peer.connections
     responder(call);
     conectar(conexaoAtual);
   }
 });
 
-
 function responder(call){
-  
     call.answer(myStream); // Responder a chamada com um stream de audio e video.
-    
     var video = document.querySelector('video#local-video');
-    video.srcObject = myStream;
-
+    video.srcObject = myStream; // adiciona o stream no campo de vídeo
+    
     video.onloadedmetadata = function (e) {
       video.play();
     };
 
-    call.on('stream', function (remoteStream) {
+    call.on('stream', function (remoteStream) { // Remote stream é o audio/video do peer emissor
       
         var video2 = document.querySelector('video#full-screen-video');
 
@@ -101,5 +94,4 @@ function responder(call){
         };
         enableUiControls();
     });
-    
 }
