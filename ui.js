5322bc7cc9056fababa11f70a5daa7befceb49d2
Vinicius Dias <vdias6849@gmail.com>
2019-11-23 22:00:18 -0300

    implementando as funções de mute video e audio

--- ui.js
+++ ui.js
@@ -1,4 +1,4 @@
-let peer = new Peer();
+
 
 setTimeout(() => {
   document.getElementById("idPeer").innerHTML ="Seu id: " + peer.id;
@@ -17,80 +17,42 @@ $( "#connect" ).click(function( event ) {
     chamar(idReceptor); //chama a função pra ligar
   });
   $("#modalForm").modal("hide");
+  
 });
 
 // UI buttons
-function enableUiControls(localStream) {
+function enableUiControls() {
 
   $("#mic-btn").prop("disabled", false);
   $("#video-btn").prop("disabled", false);
   $("#exit-btn").prop("disabled", false);
-
-  $("#mic-btn").click(function(){
-    toggleMic(localStream);
-  });
-
-  $("#video-btn").click(function(){
-    toggleVideo(localStream);
-  });
-
-  $("#screen-share-btn").click(function(){
-    toggleScreenShareBtn(); // set screen share button icon
-    $("#screen-share-btn").prop("disabled",true); // disable the button on click
-    if(screenShareActive){
-      stopScreenShare();
-    } else {
-      initScreenShare(); 
-    }
-  });
-
-  $("#exit-btn").click(function(){
-    console.log("so sad to see you leave the channel");
-    leaveChannel(); 
-  });
-
-  // keyboard listeners 
-  $(document).keypress(function(e) {
-    switch (e.key) {
-      case "m":
-        console.log("squick toggle the mic");
-        toggleMic(localStream);
-        break;
-      case "v":
-        console.log("quick toggle the video");
-        toggleVideo(localStream);
-        break; 
-      case "s":
-        console.log("initializing screen share");
-        toggleScreenShareBtn(); // set screen share button icon
-        $("#screen-share-btn").prop("disabled",true); // disable the button on click
-        if(screenShareActive){
-          stopScreenShare();
-        } else {
-          initScreenShare(); 
-        }
-        break;  
-      case "q":
-        console.log("so sad to see you quit the channel");
-        leaveChannel(); 
-        break;   
-      default:  // do nothing
-    }
-
-    // (for testing) 
-    if(e.key === "r") { 
-      window.history.back(); // quick reset
-    }
-  });
 }
+
+$("#mic-btn").click(function(){
+
+  toggleBtn($("#mic-btn")); // altera a cor do botao microfone
+  $("#mic-icon").toggleClass('fa-microphone').toggleClass('fa-microphone-slash'); // altera o icone para cortado
+  toggleMic(s);
+
+});
+
+$("#video-btn").click(function(){
+
+  toggleBtn($("#video-btn")); // altera a cor do botao video
+  $("#video-icon").toggleClass('fa-video').toggleClass('fa-video-slash'); // altera o icone para cortado
+  toggleVideo(s);
+
+});
+
+$("#exit-btn").click(function(){ // botao de desligar chamada
+  console.log("so sad to see you leave the channel");
+  desligarChamada(); 
+});
+
+
 
 function toggleBtn(btn){
   btn.toggleClass('btn-dark').toggleClass('btn-danger');
-}
-
-function toggleScreenShareBtn() {
-  $('#screen-share-btn').toggleClass('btn-danger');
-  $('#screen-share-icon').toggleClass('fa-share-square').toggleClass('fa-times-circle');
 }
 
 function toggleVisibility(elementID, visible) {
@@ -101,26 +63,24 @@ function toggleVisibility(elementID, visible) {
   }
 }
 
-function toggleMic(stream) {
-  toggleBtn($("#mic-btn")); // toggle button colors
-  $("#mic-icon").toggleClass('fa-microphone').toggleClass('fa-microphone-slash'); // toggle the mic icon
+function toggleMic(st) {
+  
   if ($("#mic-icon").hasClass('fa-microphone')) {
-    stream.getAudioTracks()[0].enabled = true; // enable the local mic
+    st.getAudioTracks()[0].enabled = true; // enable the local mic
     toggleVisibility("#mute-overlay", false); // hide the muted mic icon
   } else {
-    stream.getAudioTracks()[0].enabled = false; // mute the local mic
+    st.getAudioTracks()[0].enabled = false; // mute the local mic
     toggleVisibility("#mute-overlay", true); // show the muted mic icon
   }
 }
 
-function toggleVideo(localStream) {
-  toggleBtn($("#video-btn")); // toggle button colors
-  $("#video-icon").toggleClass('fa-video').toggleClass('fa-video-slash'); // toggle the video icon
+function toggleVideo(st) {
+  
   if ($("#video-icon").hasClass('fa-video')) {
-    localStream.unmuteVideo(); // enable the local video
+    st.getVideoTracks()[0].enabled = true;  // enable the local video
     toggleVisibility("#no-local-video", false); // hide the user icon when video is enabled
   } else {
-    localStream.muteVideo(); // disable the local video
+    st.getVideoTracks()[0].enabled = false; // disable the local video
     toggleVisibility("#no-local-video", true); // show the user icon when video is disabled
   }
 }
